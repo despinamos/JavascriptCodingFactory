fc5f18dcca19d19d1dd83e1581c639c9e364e804
D. Moschokarfi <despinamx16@gmail.com>
2025-02-14 20:58:35 +0200

    updates

---
+++ modules/src/lib2.js
@@ -0,0 +1,11 @@
+// named exports
+const PI = 3.14
+const EPSILON = 0.000005
+const API_URL = "https://codingfactory.aueb.gr"
+
+//default export - only one can be default
+const SEC_PER_MIN = 60
+export default SEC_PER_MIN
+
+// only for named exports can we can this form
+export { PI, EPSILON, API_URL }
